f08a4a88d1f87ce4afce759ac9c036e216999552
mschauer <moritzschauer@web.de>
2019-11-05 16:38:49 +0100

    Scaffold

--- src/BridgeLandmarks.jl
+++ src/BridgeLandmarks.jl
@@ -1,5 +1,22 @@
 module BridgeLandmarks
 
-greet() = print("Hello World!")
+using Bridge
+using ForwardDiff
+using Plots
+
+using LinearAlgebra
+
+const d = 2
+
+export Point, PointF, Unc, UncF
+
+include("nstate.jl")
+include("state.jl")
+include("models.jl")
+include("patches.jl")
+#include("plotlandmarks.jl")  # keep, but presently unused as all is transferred to plotting in R
+include("plotting.jl")
+include("lmguid.jl")  # replacing lmguiding_mv and update_initialstate
+
 
 end # module
